0b61b6d1dc40e8bcee6f29f18da1e4eff5927daf
Federico Puy <fedpuy@amazon.com>
2023-08-06 22:10:20 +0100

    Downgrading compileSdk from 34 to 33 Reducing version since AGP 8.1.0 was tested against v33. Upgrading AGP not possible since newer versions are not supported with Compose at the moment.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 android {
     namespace = "com.example.countriesapp"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.example.countriesapp"
